c03c4a74d203e2afc098f93ec788ff62f16c1b7f
dandan <liuyongshun2@126.com>
2019-05-13 22:26:57 +0800

    test

--- test/dealSort.spec.js
+++ test/dealSort.spec.js
@@ -1,9 +1,9 @@
-import { toggleCase } from '../src/toggleCase.js';
+import { dealSort } from '../src/dealSort.js';
 describe('first test', function() {
   it('test string that every word to be uppercase', function() {
-    assert(toggleCase('abc', 'word') === 'Abc');
+    assert(dealSort([1, 8, 2, 4], 'up') === 'Abc');
   });
   it('test the first word of each paragraph to be uppercase', function() {
-    assert(toggleCase('abc\ndcef\nghijk', 'paragraph') === 'Abc\nDcef\nGhijk');
+    assert(dealSort('abc\ndcef\nghijk', 'paragraph') === 'Abc\nDcef\nGhijk');
   });
 });
