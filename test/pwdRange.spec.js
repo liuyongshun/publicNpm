3eb78f2f7c4cd04eae9f04fe660599182ba8598e
liuyongshunu <spx-liuys@supaur.com>
2019-05-16 18:47:37 +0800

    test

--- test/pwdRange.spec.js
+++ test/pwdRange.spec.js
@@ -6,4 +6,7 @@ describe('first test', function() {
   it('password range', function() {
     assert(pwdRange('abcdef', ['length', 'number', 'upper', 'lower', 'special']).type === 'number');
   });
+  it('password range', function() {
+    assert(pwdRange('Ddd33)(', ['length', 'number', 'upper', 'lower', 'special']).status === true);
+  });
 });
--- test/rmDuplicates.spec.js
+++ test/rmDuplicates.spec.js
@@ -9,4 +9,14 @@ describe('first test', function() {
   it('Remove Duplicates', function() {
     assert(rmDuplicates([1, 3, 2, 3, 2]).indexOf(2) === 2);
   });
+   it('Remove Duplicates', function() {
+    var arr = [
+      {name: 'liu', age: 11},
+      {name: 'yong', age: 22},
+      {name: 'shun', age: 11},
+      {name: 'gou', age: 7},
+      {name: 'gou', age: 7}
+    ]
+    assert(rmDuplicates(arr, 'age').length === 3);
+  });
 });
